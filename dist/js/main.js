311992ad8357217c997d6c0e162909799e5365b1
MasriNaser <naser@foursevens.be>
2019-04-13 15:27:26 +0200

    Initail commit

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+const name = 'Brand';
+console.log('this is the output: name', name);
+name = () => {
+  console.log('hello');
+};
+console.log('object');
+const addvalu = (num1, num2) => {
+  console.log(num1 + num2);
+};
